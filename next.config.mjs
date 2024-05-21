adc6c74c372a85f73e2b6d312a6c5f43be6a8eb0
meetlimbani27 <meetlimbani23@gmail.com>
2024-05-22 03:40:39 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
+  typescript: {
+  // !! WARN !!
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  // !! WARN !!
+  ignoreBuildErrors: true,
+  },
+  }
   
   export default nextConfig;
   
