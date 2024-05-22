328e3387ec4e7901a9e75f0499c6d50c68ac0b28
meetlimbani27 <meetlimbani23@gmail.com>
2024-05-22 14:19:59 +0530

    added totop rocket button

--- components/main/ToTopBtn.tsx
+++ components/main/ToTopBtn.tsx
@@ -1,12 +1,15 @@
-"use client"
-import React, { useEffect } from 'react';
+"use client";
+import React, { useEffect, useRef } from 'react';
 import RocketSvg from '../sub/RocketSvg';
+import { motion } from 'framer-motion';
+import { slideInFromRight } from '@/utils/motion';
 
 type Props = {}
 
 const ToTopBtn = (props: Props) => {
   const [isVisible, setIsVisible] = React.useState(false);
   const [isFlying, setIsFlying] = React.useState(false);
+  const rocketRef = useRef<HTMLButtonElement | null>(null);
 
   useEffect(() => {
     const toggleVisibility = () => {
@@ -23,18 +26,16 @@ const ToTopBtn = (props: Props) => {
   }, []);
 
   useEffect(() => {
-    if (isFlying) {
-      const rocketElement = document.querySelector('.rocket') as HTMLElement;
-      if (rocketElement) {
-        rocketElement.style.transform = 'translateY(-92vh)';
-        rocketElement.style.transition = 'transform 1.8s ease-in-out';
-        setTimeout(() => {
-          rocketElement.style.transform = 'translateY(0)'; // Reset rocket position
-          setIsFlying(false);
-          window.scrollTo({ top: 0, behavior: 'smooth' });
-          setIsVisible(false); // Reset isVisible state
-        }, 1800);
-      }
+    if (isFlying && rocketRef.current) {
+      const rocketElement = rocketRef.current;
+      rocketElement.style.transform = 'translateY(-92vh)';
+      rocketElement.style.transition = 'transform 1.8s ease-in-out';
+      setTimeout(() => {
+        rocketElement.style.transform = 'translateY(0)'; // Reset rocket position
+        setIsFlying(false);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        setIsVisible(false); // Reset isVisible state
+      }, 2000);
     }
   }, [isFlying]);
 
@@ -43,14 +44,27 @@ const ToTopBtn = (props: Props) => {
   };
 
   return (
-    <div className={`fixed bottom-10 right-10 z-50 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+    <motion.div 
+    className={`fixed bottom-10 right-10 z-50 ${isVisible ? 'opacity-100' : 'opacity-0'}`}
+    initial="hidden"
+    animate={isVisible ? 'visible' : 'hidden'}
+    variants={slideInFromRight(0.5)}
+    >
+      <motion.div 
+      className=' hover:bg-slate-800 transition-all hover:rounded-full '
+      transition={{
+        duration: 1,
+      }}
+      >
       <button
         onClick={handleClick}
-        className='sm:h-16 sm:w-16 h-[30px] w-[30px] rounded-full transition-all duration-500 ease-in-out'
+        ref={rocketRef}
+        className='sm:h-16 sm:w-16 h-[30px] w-[30px] rounded-full transition-all duration-500 ease-in-out rocket '
       >
         <RocketSvg />
       </button>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
