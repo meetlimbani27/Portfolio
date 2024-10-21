8f514b36e8ba88f7c333e9db56089665c52881fc
meetlimbani27 <meetlimbani23@gmail.com>
2024-10-22 02:38:38 +0530

    hi

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,71 +1,86 @@
-"use client"
-import React from 'react';
-import { motion } from 'framer-motion';
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion';
-import { SparklesIcon } from '@heroicons/react/24/solid';
-import { ArrowDownTrayIcon } from '@heroicons/react/24/solid';
-import Image from 'next/image';
-
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import { ArrowDownTrayIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 const HeroContent = () => {
   return (
     <motion.div
-    initial='hidden'
-    animate='visible'
-    className='flex md:flex-row items-center justify-center -mt-56 sm:px-3 md:px-8 lg:px-12 xl:px-16 w-full z-10 flex-col px-3 gap-5'
+      initial="hidden"
+      animate="visible"
+      className="flex md:flex-row items-center justify-center -mt-56 sm:px-3 md:px-8 lg:px-12 xl:px-16 w-full z-10 flex-col px-3 gap-5"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
-        variants={slideInFromTop}
-        className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
-            <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
-            <h1 className='Welcome-text text-[13px]'>FullStack Developer Portfolio</h1>
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            FullStack Developer Portfolio
+          </h1>
         </motion.div>
 
         <motion.div
-        variants={slideInFromLeft(0.5)}
-        className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto z-0'
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto z-0"
         >
-            <span>
-            <span className='text-transparent bg-clip-text bg-gradient-to-l from-purple-500 to-cyan-500'> {" "} Learning {" "}</span>
-                More Ways to be
-                <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> {" "} Cool. {" "}</span>
-                
-            </span>            
+          <span>
+            <span className="text-transparent bg-clip-text bg-gradient-to-l from-purple-500 to-cyan-500">
+              {" "}
+              Learning{" "}
+            </span>
+            More Ways to be
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              Cool.{" "}
+            </span>
+          </span>
         </motion.div>
 
         <motion.p
-        variants={slideInFromLeft(0.8)}
-        className='text-lg text-gray-400 my-5 max-w-[600px] '>
-        I&apos;m a Full Stack Software Engineer With experience in Website
-        and Software development. Check out my projects and skills.
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px] "
+        >
+          I&apos;m a Full Stack Software Engineer With experience in Website
+          development. Check out my projects and skills.
         </motion.p>
 
         <motion.a
-        variants={slideInFromLeft(1)}
-        className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] duration-500  z-10'
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] duration-500  z-10"
         >
-         <a href="/CV.pdf" download={true} className='flex items-center cursor-pointer justify-center ml-2'>
-          Download CV  
-          <ArrowDownTrayIcon className='w-5 h-5 ml-3' />
-         </a> 
+          <a
+            href="/CV.pdf"
+            download={true}
+            className="flex items-center cursor-pointer justify-center ml-2"
+          >
+            Download CV
+            <ArrowDownTrayIcon className="w-5 h-5 ml-3" />
+          </a>
         </motion.a>
       </div>
-      
+
       <motion.div
-      variants={slideInFromRight(0.8)}
-      className='w-full h-full flex justify-center items-center z-10'
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center z-10"
       >
         <Image
-            src="/darkiconss.svg"
-            alt="work icons"
-            height={650}
-            width={650}
+          src="/darkiconss.svg"
+          alt="work icons"
+          height={650}
+          width={650}
         />
       </motion.div>
-
     </motion.div>
-  )
-}
+  );
+};
 
-export default HeroContent
+export default HeroContent;
--- constants/index.ts
+++ constants/index.ts
@@ -3,68 +3,73 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 
-
 export const NavLinks = [
-    {
-      title: "About Me",
-      link: "/#about"
-    },
-    {
-        title: "Skills",
-        link: "/#skills"
-    },
-    {
-        title: "Projects",
-        link: "/#projects"
-    },
-    {
-        title: "Experience",
-        link: "/#experience"
-    },
-    {
-        title: "Contact",
-        link: "/#contact"
-    },
-  ];
+  {
+    title: "About Me",
+    link: "/#about",
+  },
+  {
+    title: "Skills",
+    link: "/#skills",
+  },
+  {
+    title: "Projects",
+    link: "/#projects",
+  },
+  {
+    title: "Experience",
+    link: "/#experience",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+];
 
-  export const Socials = [
-    {
-      name: "LinkdIn",
-      src: "/icons/linkedin.svg",
-      link: "https://www.linkedin.com/in/meet-limbani/"
-    },
-    {
-      name: "GitHub",
-      src: "/icons/github.svg",
-      link: "https://github.com/meetlimbani27"
-    },
-    
-  ];
+export const Socials = [
+  {
+    name: "LinkdIn",
+    src: "/icons/linkedin.svg",
+    link: "https://www.linkedin.com/in/meet-limbani/",
+  },
+  {
+    name: "GitHub",
+    src: "/icons/github.svg",
+    link: "https://github.com/meetlimbani27",
+  },
+];
 
-  export const experiencesData = [
-    {
-      title: "BE - CE",
-      location: "LDRP-ITR, Gandhinagar, Gujarat",
-      description:
-        "Pursuing bachelor's degree in CE engineering.",
-      icon: React.createElement(LuGraduationCap),
-      date: "2021 - 2025",
-    },
-    
-    {
-      title: "Full-Stack Developer",
-      location: "The Special Character, Ahmedabad, Gujarat",
-      description:
-        ' We a team of 6, worked diligiently to make a fully complete ecommerce website for fruits,vegetables and everyday groceries which generates orderid also. We used React, Next.js14, Typescript, Tailwind and GitHub for version control and seemless collaboration which gave us a great experience and recognition on the stage among many fellow developers. This project is the fruit of our great Teamwork',
-      icon: React.createElement(CgWorkAlt),
-      date: "Dec 2023 - Jan 2024",
-    },
-    {
-      title: "Back-end Developer",
-      location: "SSIP Hackathon",
-      description:
-        'I worked on a web project named "GIFT Connect" which is a platform for the residents, employees and managing authority of the GIFT City in Gujarat which is a Financial City. My Job as a backend dev was to create backend for news, events, polls, voting system and unique tag for residents, employees and managing authority. And make a admin page which will allow the managing authority to manage the website.',
-      icon: React.createElement(FaReact),
-      date: "Nov 2023",
-    },
-  ] as const;
+export const experiencesData = [
+  {
+    title: "BE - CE",
+    location: "LDRP-ITR, Gandhinagar, Gujarat",
+    description: "Pursuing bachelor's degree in CE engineering.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2021 - 2025",
+  },
+
+  {
+    title: "Associate Software Developer",
+    location: "Openxcell | Next-Gen AI Services",
+    description:
+      "Working as a Associate Software Developer in Openxcell | Next-Gen AI Services and working with  Next.js , Typescript , Tailwind and GitHub. Along with Learning AI , AI workflows , cloud computing, prompting and many more.",
+    icon: React.createElement(CgWorkAlt),
+    date: "October 2024 - Present",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "The Special Character, Ahmedabad, Gujarat",
+    description:
+      " We a team of 6, worked diligiently to make a fully complete ecommerce website for fruits,vegetables and everyday groceries which generates orderid also. We used React, Next.js14, Typescript, Tailwind and GitHub for version control and seemless collaboration which gave us a great experience and recognition on the stage among many fellow developers. This project is the fruit of our great Teamwork",
+    icon: React.createElement(CgWorkAlt),
+    date: "Dec 2023 - Jan 2024",
+  },
+  {
+    title: "Back-end Developer",
+    location: "SSIP Hackathon",
+    description:
+      'I worked on a web project named "GIFT Connect" which is a platform for the residents, employees and managing authority of the GIFT City in Gujarat which is a Financial City. My Job as a backend dev was to create backend for news, events, polls, voting system and unique tag for residents, employees and managing authority. And make a admin page which will allow the managing authority to manage the website.',
+    icon: React.createElement(FaReact),
+    date: "Nov 2023",
+  },
+] as const;
